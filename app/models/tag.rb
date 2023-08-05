1066802a4df0c78e3b1499655970cfe2c4879d47
Trevor Gaffney <tgaffer@gmail.com>
2023-08-05 16:12:25 +0900

    Added optional tag params

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ApplicationRecord
   belongs_to :country, optional: true
   belongs_to :topic, optional: true
   belongs_to :article, optional: true
+
+  validates :country, :topic, :article,
+            presence: true,
+            unless: -> { !topic.blank? || !article.blank? || !country.blank? }
 end
