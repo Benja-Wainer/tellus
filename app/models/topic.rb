36f5c4355e6f353d440db6a64f03a6564f24987b
Benjamin Wainer <benjawainer@gmail.com>
2023-08-07 13:09:35 +0900

    Added show page for articles (no banner yet). Added article scss file. Also added migration for article sources

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
-  has_many :articles, through: :tags
   has_many :tags
+  has_many :articles, through: :tags
   has_many :countries, through: :tags
 
   # acts_as_favoritable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_054741) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_021932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_054741) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
   create_table "countries", force: :cascade do |t|
@@ -29,9 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_054741) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.bigint "country_id"
-    t.bigint "topic_id"
-    t.bigint "article_id"
+    t.bigint "country_id", null: false
+    t.bigint "topic_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_tags_on_article_id"
