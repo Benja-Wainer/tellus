a6257e677be84697fc14defbc43f6ba52db43647
Trevor Gaffney <tgaffer@gmail.com>
2023-08-10 22:55:34 +0900

    Incorporated favoritor and heart toggles

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,7 @@ import { Application } from "@hotwired/stimulus"
 import Reveal from 'stimulus-reveal-controller'
 
 const application = Application.start()
+application.register('reveal', Reveal)
 
 // Configure Stimulus development experience
 application.debug = false
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_105831) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
   create_table "countries", force: :cascade do |t|
