52a331d51822e46bb0bc6c29931cabc07c891c35
Christian Stprix <christprix@gmail.com>
2023-08-10 20:21:22 +0900

    added search function for countries

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+gem 'pg_search'
+
 gem "json"
 
 gem 'net-http'
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,9 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+    if params[:query].present?
+      @topics = Topic.search_by_name(params[:query])
+    else
+      @topics = Topic.all
+    end
   end
 end
